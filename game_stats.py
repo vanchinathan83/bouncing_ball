aa378e1683d13ba90f7c2cf1f3ad8a425fcdb903
Vanchinathan <vayipalayamchand@paypal.com>
2020-01-26 08:03:54 -0600

    Changing the speed at which the game progresses

--- game_stats.py
+++ game_stats.py
@@ -8,14 +8,20 @@ class GameStats:
         self.is_game_active = True
         self.score = 0
         self.no_of_lives = self.settings.max_lives
+        self.score_checkpoint = 1000
 
     def update_score(self):
         """ Updates the score of the game. """
-        self.score += self.settings.ball_speed * self.settings.no_of_balls
+        self.score += round(float(self.settings.ball_speed *
+                                  self.settings.no_of_balls * .2))
+
+        if self.score > self.score_checkpoint * self.settings.no_of_balls:
+            self.settings.ball_speed += 1
+            self.settings.board_speed += 4
+            self.score_checkpoint += 1000
+
         if self.score > 5000 * self.settings.no_of_balls:
             self.settings.no_of_balls += 1
-            self.settings.ball_speed += 5
-            self.settings.board_speed += 20
 
     def decrement_lives(self):
         if self.no_of_lives:
